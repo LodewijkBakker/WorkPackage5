0a519b32cbd15c5996ef43cd368424e627b58095
LodewijkBakker <75375396+LodewijkBakker@users.noreply.github.com>
2020-12-02 20:07:34 +0100

    v1.2 Added preliminary sheet dimension function

--- 53.py
+++ 53.py
@@ -35,10 +35,10 @@ def BucklingDimensions(v_min, E, stress_min, rho, t_1, t_2):
         m_structure = MassStructure(rho, r_tank, t_1, t_2, L_tank)
         # higher r can be made but if volume is to low it is no good.
 
-        v_new = (4/3)*np.pi*r_tank**3 + np.pi*r_tank**2*L_tank #thin walled
+        v_new = (4/3)*np.pi*r_tank**3 + np.pi*r_tank**2*L_tank  # thin walled
         if v_new < v_min:
             r_forv_list = np.roots([(4/3)*np.pi, np.pi*L_tank, 0, -v_min])
-            #check for lowest possible positive real root
+            # check for lowest possible positive real root
 
             for j, i in enumerate(r_forv_list.imag):
                 if i == 0 and r_forv_list.real[j] > 0:
@@ -67,36 +67,42 @@ def ShellDimensions(v_min, E, stress_min, pressure, p_ratio, rho):
     r_min = 0.1
     r_max = 1.2
     t_1_min = 0.01
-    t_1_max = 0.06
 
-    prop_list = dict(L_tank_list=[], r_tank_list=[], t_1_list=[], m_list=[])
+    prop_list = dict(L_tank_list=[], r_tank_list=[], t_1_list=[], t_2_list=[], m_list=[])
 
     AcDif = 0.01  # accuracy of the dimension in [m]
     for L_tank in np.arange(L_min, L_max, AcDif):
         for r_tank in np.arange(r_min, r_max, AcDif):
-            v_new = (4 / 3) * np.pi * r_tank ** 3 + np.pi * r_tank ** 2 * L_tank #thin walled assumption
+            v_new = (4 / 3) * np.pi * r_tank ** 3 + np.pi * r_tank ** 2 * L_tank
+            # thin walled assumption
             if v_new > v_min:
                 t_1 = t_1_min
-                while t_1 < t_1_max:
+                ShellFailure = True
+                while ShellFailure:
                     stress_criticals = MaxShellBuckling(pressure, E, r_tank, t_1, L_tank, p_ratio)
                     if stress_min < stress_criticals:
+                        ShellFailure = False
                         t_2 = t_1 * 1.2
                         # (important because due to pressure there is some ratio that works
                         # for not warping the whole pressure vessel is sort of assumption)
 
                         m_structure = MassStructure(rho, r_tank, t_1, t_2, L_tank)
+                        prop_list["m_list"].append(m_structure)
                         prop_list["L_tank_list"].append(L_tank)
                         prop_list["r_tank_list"].append(r_tank)
-                        prop_list["m_list"].append(m_structure)
+                        prop_list["t_1_list"].append(t_1)
+                        prop_list["t_2_list"].append(t_2)
                         # add it to dictionary or class
                     else:
-                        x = 0 # placeholder
+                        t_1 += AcDif
 
-                    t_1 += AcDif
-
-
-
-    #binary elemination
+    min_mass_pos = prop_list["m_list"].index(min(prop_list["m_list"]))
+    L_tank_new = prop_list["L_tank_list"][min_mass_pos]
+    r_tank_new = prop_list["r_tank_list"][min_mass_pos]
+    t_1_tank_new = prop_list["t_1_list"][min_mass_pos]
+    t_2_tank_new = prop_list["t_2_list"][min_mass_pos]
+    # this selects for the best
+    return L_tank_new, r_tank_new, t_1_tank_new, t_2_tank_new
 
 
 def StressExperienced(r_tank, t_1, t_2, rho, L_tank, acc_rocket, m_fuel, pressure):
@@ -136,8 +142,7 @@ def MaxShellBuckling(pressure, E, r_tank, t_1, L_tank, p_ratio):
     # can be easily received if one calculates differentiation where a minimum is equal to 0
     k_factor = lmd_half + constant_k*(1/lmd_half)
 
-    stress_criticals = (1.983 - 0.983 * np.exp(-23.14 * Q_factor)) * \
-                       k_factor * ((E * np.pi ** 2) / (12 * (1 - p_ratio ** 2))) * (t_1 / L_tank) ** 2
+    stress_criticals = (1.983 - 0.983 * np.exp(-23.14 * Q_factor)) * k_factor * ((E * np.pi ** 2) / (12 * (1 - p_ratio ** 2))) * (t_1 / L_tank) ** 2
     # critical shell buckling (e or scientific notation)
     # oh exp can return imaginary values if not positive so be aware!!
     print(stress_criticals)
@@ -172,8 +177,8 @@ def InputVal():
         if stress_criticalb < stress_criticals:  # bstress is lower
             L_tank_new, r_tank_new = BucklingDimensions(v_min, E, stress_axial, rho, t_1, t_2)
 
-        else: # else stress_criticalb > stress_criticals important may be cause for errors
-            x = 2
+        else:  # else stress_criticalb > stress_criticals important may be cause for errors
+            L_tank_new, r_tank_new, t_1_tank_new, t_2_tank_new = ShellDimensions(v_min, E, stress_axial, pressure, p_ratio, rho)
 
 
 InputVal()
