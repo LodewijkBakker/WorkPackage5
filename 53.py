f31cb0bfb83529d9c7db857772175d657462631d
msahirs <sujahudeen.sahir@gmail.com>
2020-12-04 09:00:08 +0100

    test push

--- 53.py
+++ 53.py
@@ -1,7 +1,6 @@
 import numpy as np
 # preliminary dimensions
-#Hi there`! - Sahir
-#T2www
+#test push
 def CrossSectionArea(r_tank, t_1):
     # cross sectional area is needed for allot of part this makes it so that cross sectionc
     # can be changed everywhere if incorrect
